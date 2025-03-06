a9ebc274aef07ab8487c4b862c56921ce9a0d455
Tom Okafor <tmeka008@gmail.com>
2025-03-06 22:57:34 +0100

    feat: add and style features, testimonial and footer sections on the coming soon page.

--- app/coming-soon/page.tsx
+++ app/coming-soon/page.tsx
@@ -4,13 +4,26 @@ import twitter from "@/public/icons/twitter.svg";
 import facebook from "@/public/icons/facebook.svg";
 import instagram from "@/public/icons/instagram.svg";
 import logo from "@/public/icons/logo.svg";
-import play from '@/public/coming-soon/Icon.svg'
+import play from "@/public/coming-soon/Icon.svg";
+import left from "../../public/coming-soon/_ui-arrow-narrow-left.svg";
+import right from "../../public/coming-soon/_ui-arrow-right.svg";
+import testimonial from "../../public/coming-soon/Container.png";
 import Image from "next/image";
+import { Features } from "@/components/shared/features";
 
 const Page = () => {
   const socialLinkButtonClass: string =
     "w-[40px] h-[40px] bg-white border-[1px] rounded-full border-[#d0d0d0] inline-flex justify-center items-center";
   const sharedClass: string = "flex flex-col items-center";
+  const countDownBlockClass: string =
+    "flex justify-center items-center gap-[12px]";
+  const countDownNumberClass: string =
+    "text-[56px] leading font-[500] text-center text-white capitalize";
+  const countDownTextClass: string =
+    "text-[36px] leading-[46px] text-[#dedede] text-center font-[500] capitalize";
+  const countDownColonClass: string =
+    "text-[56px] leading-[71px] font-[500] text-center text-[#dedede]";
+
   return (
     <>
       <header className="h-20 flex items-center justify-between px-[70px] mb-[106px]">
@@ -69,14 +82,79 @@ const Page = () => {
             <p className="text-[17px] text-white font-[500]">ShopDesk</p>
           </div>
           <div className="rounded-[12px] p-3 flex items-center bg-[#00000066] h-[53.5px] w-[53.5px] my-auto">
-            <Image src={play} alt="play button" width={30} priority  />
+            <Image src={play} alt="play button" width={30} priority />
           </div>
         </div>
       </section>
-      <section></section>
-      <section></section>
-      <section></section>
-      <footer></footer>
+
+      <section className="flex items-center justify-center bg-[#19A45B] h-[185px] mb-[96px]">
+        <div className="flex justify-between gap-8 px-8 items-center">
+          <div className={countDownBlockClass}>
+            <p className={countDownNumberClass}>24</p>
+            <p className={countDownTextClass}>days</p>
+          </div>
+          <p className={countDownColonClass}>:</p>
+          <div className={countDownBlockClass}>
+            <p className={countDownNumberClass}>24</p>
+            <p className={countDownTextClass}>hours</p>
+          </div>
+          <p className={countDownColonClass}>:</p>
+          <div className={countDownBlockClass}>
+            <p className={countDownNumberClass}>24</p>
+            <p className={countDownTextClass}>minutes</p>
+          </div>
+          <p className={countDownColonClass}>:</p>
+          <div className={countDownBlockClass}>
+            <p className={countDownNumberClass}>24</p>
+            <p className={countDownTextClass}>seconds</p>
+          </div>
+        </div>
+      </section>
+
+      <Features text="FEATURES ROLLING IN" />
+
+      <section className="bg-[#E5F5ED] flex flex-col items-center gap-[40px] py-8">
+        <div className="w-[min(90%,1212px)] flex justify-between items-center ">
+          <button className="size-[48px] p-[15px] rounded-[12px] border-[1px] border-[#C7D3E1] bg-white flex justify-center items-center">
+            <Image src={left} alt="left button" height={18} width={18} />
+          </button>
+          <p className="text-[#009A49] text-[16px] leading-[24px] font-[500]">
+            Reviews from Beta Testers
+          </p>
+
+          <button className="size-[48px] p-[15px] rounded-[12px] bg-[#001A00] flex justify-center items-center">
+            <Image src={right} alt="left button" height={18} width={18} />
+          </button>
+        </div>
+        <p className="text-[32px] text-center font-[450] leading-[42px] text-black max-w-[50ch]">
+          Shop Desk transformed how we manage our stock – it's a game changer.
+          Finally, a retail management solution that is intuitive and scalable.
+          I can’t wait for the full launch!
+        </p>
+        <p className="text-[32px] text-center font-[450] leading-[42px] text-black max-w-[50ch]">
+          — Lisa M, Small Business Owner
+        </p>
+        <Image
+          src={testimonial}
+          alt="testimonial polariod"
+          height={80}
+          width={80}
+        />
+      </section>
+
+      <footer className="py-[41px] px-[72px] flex justify-center items-center content-center bg-white">
+        <div className="h-[61px] w-full flex justify-between items-end">
+          <p className="text-[#71717a] text-[14px] leading-[22px] font-[400] text-center">
+            © Copyright 2024, Powered by Timbu Business
+          </p>
+          <div className="flex gap-[54px] items-center *:text-[#71717a] *:text-[14px] *:leading-[22px] *:font-[400] *:text-center">
+            <p>Cookies</p>
+            <p>Terms of Sevice</p>
+            <p>Privacy Policy</p>
+            <p>Manage privacy</p>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
