befc8c163c5a9cc90c34c0d7a0ab53f1cc219b7f
Siddami <dharmmy621@gmail.com>
2025-03-12 08:47:20 +0100

    fix: positioning of pagination in table

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -359,6 +359,7 @@ const Page = () => {
                       </TableCell>
                     </TableRow>
                   ))}
+
                   {/* Add empty rows to maintain table height if needed */}
                   {emptyRowsCount > 0 &&
                     Array.from({ length: emptyRowsCount }).map((_, index) => (
@@ -370,9 +371,26 @@ const Page = () => {
                         <TableCell className="px-4 py-3 text-center hidden sm:table-cell"></TableCell>
                       </TableRow>
                     ))}
+                    
+                    <TableCell colSpan={5}>
+                    <PaginationFeature
+                          totalItems={totalItems}
+                          currentPage={currentPage}
+                          itemsPerPage={rowsPerPage}
+                          totalPages={totalPages}
+                          onPageChange={handlePageChange}
+                          onItemsPerPageChange={handleItemsPerPageChange}
+                        />
+                    </TableCell>
+
                 </TableBody>
+                 
               </Table>
             )}
+              
+             
+                      
+                   
           </div>
         </div>
       </div>
@@ -385,16 +403,7 @@ const Page = () => {
       />
 
       <div className="flex flex-col gap-2 mt-4">
-        <div className="bg-[#DEE5ED] p-2 flex items-center justify-between">
-          <PaginationFeature
-            totalItems={totalItems}
-            currentPage={currentPage}
-            itemsPerPage={rowsPerPage}
-            totalPages={totalPages}
-            onPageChange={handlePageChange}
-            onItemsPerPageChange={handleItemsPerPageChange}
-          />
-        </div>
+       
 
         <p className="text-center mt-4">
           © {new Date().getFullYear()}, Powered by Timbu Business
